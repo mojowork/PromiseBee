4250c8c5f89ed558704c1781369f9d74a25c5b5c
chaoshuai <huangchsh@ersoft.cn>
2021-01-28 16:36:33 +0800

    fix: 规范代码

--- src/index.js
+++ src/index.js
@@ -1,121 +1,88 @@
+const PADDING = 'padding'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
 
-const pending = 'pending',
-    fulfilled = 'fulfilled',
-    rejected = 'rejected',
-    emptyFn = () => {}
-
-export default class PromiseBee{
+class PromiseBee{
     constructor(executor) {
-        this.state = pending
-        this.value = null
-        this.resolvedfns = []
-        this.rejectedfns = []
-        const resolve = (value) => {
-            if(this.state !== pending) return 
-            this.state = fulfilled
-            this.value = value
-            while(this.resolvedfns.length){
-                let onResolved = this.resolvedfns.pop()
+        this.PromiseState = PADDING
+        this.PromiseResult = null
+        this.PromiseFns = []
+        const resolve = value => {
+            // console.log('resolve', this)
+            if(this.PromiseState !== PADDING) return
+            this.PromiseState = FULFILLED
+            this.PromiseResult = value
+            for (const obj of this.PromiseFns) {
+                setTimeout(() => {
+                    obj.onResolved()
+                })
                 
-                return new PromiseBee((resolved, rejected) => {
-                    try{
-                        var result = onResolved(this.value)
-                    } catch (err){
-                        rejected(err)
-                    }
-                    if(result instanceof PromiseBee){
-                        result.then(resolved, rejected)
-                    } else {
-                        resolved(result)
-                    }
-                })   
             }
         }
-        const reject = (error) => {
-            if(this.state !== pending) return 
-            this.state = rejected
-            this.value = error
-            while(this.rejectedfns.length){
-                let onRejected = this.rejectedfns.pop()
-                return new PromiseBee((resolved, rejected) => {
-                    try{
-                        var result = onRejected(this.value)
-                    } catch (err){
-                        rejected(err)
-                    }
-                    if(result instanceof PromiseBee){
-                        result.then(resolved, rejected)
-                    } else {
-                        resolved(result)
-                    }
-                })               
+        const reject = resoon => {
+            // console.log('reject', this)
+            if(this.PromiseState !== PADDING) return
+            this.PromiseState = REJECTED
+            this.PromiseResult = resoon
+            for (const obj of this.PromiseFns) {
+                setTimeout(() => {
+                    obj.onRejected()
+                })
             }
         }
 
         try{
             executor(resolve, reject)
-        } catch(err) {
-            reject(err)
+        } catch (error) {
+            reject(error)
         }
+        
     }
 
-    then(onResolved = emptyFn, onRejected = emptyFn) {
-        if(this.state === pending){
-            this.resolvedfns.push(onResolved)
-            this.rejectedfns.push(onRejected)
-        } else if(this.state === fulfilled)  {
-            
-            return new PromiseBee((resolved, rejected) => {
-                try{
-                    var result = onResolved(this.value)
-                } catch (err){
-                    rejected(err)
+    then(onResolved, onRejected) {
+        debugger
+        onResolved = typeof onResolved === 'function' ? onResolved : value => value
+        onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
+        return new PromiseBee((resoved, rejected) => {
+            const walk = callback => {
+                // console.log('walk', callback)
+                try {
+                    const result = callback(this.PromiseResult)
+                    if(result instanceof PromiseBee){
+                        result.then(resoved, rejected)
+                    } else {
+                        resoved(result)
+                    }
+                } catch (error) {
+                    rejected(error)
                 }
-                if(result instanceof PromiseBee){
-                    result.then(resolved, rejected)
-                } else {
-                    resolved(result)
-                }
-            })
-        } else if(this.state === rejected){
-            return new PromiseBee((resolved, rejected) => {
-                try{
-                    var result = onRejected(this.value)
-                } catch (err){
-                    rejected(err)
-                }
-                if(result instanceof PromiseBee){
-                    result.then(resolved, rejected)
-                } else {
-                    resolved(result)
-                }
-            })
-        }
+            }
+
+            if(this.PromiseState === PADDING){
+                this.PromiseFns.push({
+                    onResolved: () => {
+                        walk(onResolved)
+                    },
+                    onRejected: () => {
+                        walk(onRejected)
+                    }
+                })
+
+            } else if(this.PromiseState === FULFILLED){
+                setTimeout(() => {
+                    walk(onResolved)
+                })
+            } else {
+                setTimeout(() => {
+                    walk(onRejected)
+                })
+            }
+
+        })
     }
 
 }
 
-console.log('PromiseBee', PromiseBee)
-let p = new PromiseBee((resolved, rejected) => {
-    setTimeout(() => {
-        let number = Math.random()
-        if(number > .5){
-            resolved(number)
-        } else {
-            rejected(number)
-        }
-    }, 5000);
-    // let number = Math.random()
-    // if(number > .5){
-    //     resolved(number)
-    // } else {
-    //     rejected(number)
-    // }
-})
-let p2 = p.then(value => {
-    throw new Error('Error')
-}, err => {
-    console.log('err ==>', err)
-})
 
-console.log('instance', p2)
+window.PromiseBee = PromiseBee
+// window.PromiseBee = Promise
